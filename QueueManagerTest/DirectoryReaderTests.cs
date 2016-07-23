11d0a31e79adaeaf97fb2d301c70008c1b3796cd
Taylor <taylorglenn@live.com>
2016-07-23 18:21:05 -0500

    Made Directory Reader tests not depend on any file being in the project directory.  Instead, during the setup, the folder and files are created, and then deleted in the teardown.

--- QueueManagerTest/DirectoryReaderTests.cs
+++ QueueManagerTest/DirectoryReaderTests.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using NUnit.Framework;
 using QueueManager;
+using System.IO;
+using System.Text;
+using System;
+
 
 namespace QueueManagerTest
 {
@@ -8,19 +12,31 @@ namespace QueueManagerTest
     public class DirectoryReaderTests
     {
         private DirectoryReader _directoryReader;
-        private string path;
+        private string upperLevelFolderName;
+        private string pathString;
+        private string appendedPathString;
+        private string[] filesInUpperLevelFolder = new string[4] { "a1.iso", "a2.iso", "a3.iso", "a4.iso - Shortcut.lnk" };
 
         [SetUp]
         public void SetUp()
         {
             _directoryReader = new DirectoryReader();
-            path = @"..\..\TestDir\";
+            upperLevelFolderName = @"..\..\TestDir\";
+            pathString = System.IO.Path.GetFullPath(upperLevelFolderName);
+            //Create folder
+            System.IO.Directory.CreateDirectory(pathString);
+            //Create files in folder
+            for(int i = 0; i < filesInUpperLevelFolder.Length; i++)
+            {
+                appendedPathString = System.IO.Path.Combine(pathString, filesInUpperLevelFolder[i]);
+                System.IO.File.Create(appendedPathString);
+            }
         }
 
         [Test]
         public void TestAddFilesInDirectoryToList()
         {
-            Assert.AreEqual(new List<string> { { path + "a1.iso" }, { path + "a2.iso" }, { path + "a3.iso" }, { path + "a4.iso - Shortcut.lnk" } }, _directoryReader.GetListOfMoviesFromDirectory(path));
+            Assert.AreEqual(new List<string> { { upperLevelFolderName + "a1.iso" }, { upperLevelFolderName + "a2.iso" }, { upperLevelFolderName + "a3.iso" }, { upperLevelFolderName + "a4.iso - Shortcut.lnk" } }, _directoryReader.GetListOfMoviesFromDirectory(upperLevelFolderName));
         }
 
         [Test]
@@ -32,15 +48,23 @@ namespace QueueManagerTest
         [Test]
         public void TestCreateDictionaryForNamesAndPaths()
         {
-            Assert.AreEqual(new Dictionary<string, string> { { path + "a1.iso", "a1" }, { path + "a2.iso", "a2" }, { path + "a3.iso", "a3" }, { path + "a4.iso - Shortcut.lnk", "a4" } },
-                _directoryReader.CreateDictionaryForNamesAndPaths(_directoryReader.GetListOfMoviesFromDirectory(path)));
+            Assert.AreEqual(new Dictionary<string, string> { { upperLevelFolderName + "a1.iso", "a1" }, { upperLevelFolderName + "a2.iso", "a2" }, { upperLevelFolderName + "a3.iso", "a3" }, { upperLevelFolderName + "a4.iso - Shortcut.lnk", "a4" } },
+                _directoryReader.CreateDictionaryForNamesAndPaths(_directoryReader.GetListOfMoviesFromDirectory(upperLevelFolderName)));
         }
 
         [Test]
         public void TestGetListOfFileNames()
         {
-            Dictionary<string, string> dictionaryOfPathsAndNames = _directoryReader.CreateDictionaryForNamesAndPaths(_directoryReader.GetListOfMoviesFromDirectory(path));
+            Dictionary<string, string> dictionaryOfPathsAndNames = _directoryReader.CreateDictionaryForNamesAndPaths(_directoryReader.GetListOfMoviesFromDirectory(upperLevelFolderName));
             Assert.AreEqual(new List<string> { { "a1" }, { "a2" }, { "a3" }, { "a4" } }, _directoryReader.GetListOfFileNames(dictionaryOfPathsAndNames));
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            System.GC.Collect();
+            System.GC.WaitForPendingFinalizers();
+            //delete directory
+            System.IO.Directory.Delete(pathString, true);
         }
     }
 }
